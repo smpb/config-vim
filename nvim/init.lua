af884de2500d9741d7ecfb075e20830c9916eed8
Sérgio Bernardino <me@sergiobernardino.net>
2024-01-22 23:21:01 +0000

    Added keymaps focused on macOS and Neovide.

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,3 +13,28 @@ vim.cmd([[
   source ~/.vimrc
 ]])
 
+
+-- ========[ NEOVIDE SETTINGS ]========
+
+-- allow the use of Cmd on macOS
+vim.g.neovide_input_use_logo = 1
+
+-- map <D-a> to select all
+vim.api.nvim_set_keymap('t', '<D-a>', 'ggVG',    { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<D-a>', 'ggVG',    { noremap = true, silent = true })
+
+-- map <D-c> to copy to system clipboard
+vim.api.nvim_set_keymap('n', '<D-c>', '"+y<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<D-c>', '"+y<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<D-c>', '"+y<CR>', { noremap = true, silent = true })
+
+-- map <D-x> to cut to system clipboard
+vim.api.nvim_set_keymap('n', '<D-x>', '"+d<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<D-x>', '"+d<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<D-x>', '"+d<CR>', { noremap = true, silent = true })
+
+-- map <D-v> to paste from system clipboard
+vim.api.nvim_set_keymap('n', '<D-v>', '"+p<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<D-v>', '"+p<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<D-v>', '"+p<CR>', { noremap = true, silent = true })
+

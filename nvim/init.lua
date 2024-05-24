e591c53f210aa4a47e662b88bfa75ca307bcf48a
Sérgio Bernardino <me@sergiobernardino.net>
2024-05-24 17:57:50 -0400

    Updated the 'Neovide' options block.

--- nvim/init.lua
+++ nvim/init.lua
@@ -79,11 +79,22 @@ vim.api.nvim_set_keymap('v', '<Leader>o', ':Oatmeal<CR>', { noremap = true, sile
 
 -- ========[ NEOVIDE SETTINGS ]========
 
--- allow the use of Cmd on macOS
-vim.g.neovide_input_use_logo = true
+if vim.g.neovide then
+  -- allow the use of Cmd on macOS
+  vim.g.neovide_input_use_logo = true
 
--- interprets <Alt+whatever> as <M-whatever>
-vim.g.neovide_input_macos_option_key_is_meta = 'only_left'
+  -- interprets <Alt+whatever> as <M-whatever>
+  vim.g.neovide_input_macos_option_key_is_meta = 'only_left'
+
+  -- hide mouse when typing
+  vim.g.neovide_hide_mouse_when_typing = true
+
+  -- use the window size from the previous session
+  vim.g.neovide_remember_window_size = true
+
+  -- produce particles behind the cursor
+  vim.g.neovide_cursor_vfx_mode = "torpedo"
+end
 
 -- map <D-a> to select all
 vim.api.nvim_set_keymap('n', '<D-a>', 'ggVG', { noremap = true, silent = true })

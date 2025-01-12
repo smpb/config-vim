29d109e4e40c3ed9ce6c80ed24b55898fc97a547
Sérgio Bernardino <me@sergiobernardino.net>
2025-01-12 19:16:23 +0000

    Replaced the plugin 'nerdtree' with 'nvim-tree'.

--- nvim/init.lua
+++ nvim/init.lua
@@ -76,7 +76,18 @@ local lazyplugins = {
   { 'tpope/vim-surround' },
 
   -- file management
-  { 'preservim/nerdtree' },
+  {
+    'nvim-tree/nvim-tree.lua',
+    lazy = true,
+    cmd  = 'NvimTreeToggle',
+    init = function()
+      -- disable netrw
+      vim.g.loaded_netrw       = 1
+      vim.g.loaded_netrwPlugin = 1
+      -- set termguicolors to enable highlight groups
+      vim.opt.termguicolors = true
+    end,
+  },
   {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
@@ -190,6 +201,28 @@ vim.keymap.set('i', '<M-Tab>', 'copilot#Accept("\\<Tab>")', {
   expr = true,
   replace_keycodes = false
 })
+
+
+-- ========[ NVIM-TREE SETTINGS ]========
+
+require('nvim-tree').setup({
+  on_attach = function(bufnr)
+    vim.api.nvim_set_hl(0, 'NvimTreeNormal',      { bg = '#282C34' })
+    vim.api.nvim_set_hl(0, 'NvimTreeEndOfBuffer', { bg = '#282C34' })
+  end,
+  sort = {
+    sorter = 'case_sensitive',
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+vim.keymap.set('n', '<Leader>d', '<CMD>NvimTreeToggle<CR>', { desc = 'Toggle file tree' })
+vim.keymap.set('v', '<Leader>d', '<CMD>NvimTreeToggle<CR>', { desc = 'Toggle file tree' })
 
 
 -- ========[ ONEDARK SETTINGS ]========

2c899debf4305a85baf433be71af179a2ef37042
Sérgio Bernardino <me@sergiobernardino.net>
2025-02-24 10:51:34 +0000

    Tweaked the list of Treesitter parsers that are installed.

--- nvim/init.lua
+++ nvim/init.lua
@@ -53,8 +53,9 @@ local lazyplugins = {
           'bash', 'c', 'comment', 'css', 'csv', 'diff', 'dockerfile',
           'git_config', 'git_rebase', 'gitattributes', 'gitcommit', 'gitignore',
           'go', 'gomod', 'gosum', 'gotmpl', 'helm', 'html', 'http', 'javascript',
-          'jq', 'json', 'lua', 'make', 'perl', 'php', 'regex', 'sql', 'ssh_config',
-          'tmux', 'toml', 'typescript', 'vim', 'vimdoc', 'xml', 'yaml'
+          'jq', 'json', 'lua', 'markdown', 'markdown_inline', 'make', 'perl',
+          'php', 'regex', 'sql', 'ssh_config', 'tmux', 'toml', 'typescript',
+          'vim', 'vimdoc', 'xml', 'yaml'
         },
         sync_install     = false,
         highlight        = { enable = true },

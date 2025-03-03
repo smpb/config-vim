16134545a86436a512b6cb5be7935e5ea19b9ae4
Sérgio Bernardino <me@sergiobernardino.net>
2025-03-03 11:43:24 +0000

    Disabled ALE's virtual text messages.

--- nvim/lua/plugins/specs/ale.lua
+++ nvim/lua/plugins/specs/ale.lua
@@ -3,6 +3,8 @@ return {
 
   -- configuration
   config = function()
+    vim.g.ale_virtualtext_cursor = 'disabled'
+
     vim.api.nvim_create_autocmd('FileType', {
       pattern = { 'go', 'helm' },
       callback = function()

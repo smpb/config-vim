a273b7e05508475c44da906ca2d85f3851af20c4
Sérgio Bernardino <me@sergiobernardino.net>
2025-03-03 00:47:53 +0000

    Fixed problems with the 'paste_in_insert_mode' function.

--- nvim/lua/core/functions.lua
+++ nvim/lua/core/functions.lua
@@ -4,24 +4,25 @@ local M = {}
 
 -- handle pasting in insert mode
 M.paste_in_insert_mode = function()
-  if vim.o.paste then
-    local original_autoindent  = vim.bo.autoindent
-    local original_smartindent = vim.bo.smartindent
+  local original_autoindent  = vim.bo.autoindent
+  local original_smartindent = vim.bo.smartindent
 
-    -- Temporarily disable autoindent and smartindent
-    vim.wo.autoindent  = false
-    vim.wo.smartindent = false
+  -- Temporarily disable autoindent and smartindent
+  vim.bo.autoindent  = false
+  vim.bo.smartindent = false
 
-    vim.api.nvim_command('set nopaste') -- Temporarily disable paste mode
-    vim.api.nvim_input('<C-r>+')        -- Paste from the system clipboard
-    vim.api.nvim_command('set paste')   -- Re-enable paste mode
+  vim.opt.paste = true
 
-    -- Reset autoindent and smartindent
-    vim.wo.autoindent  = original_autoindent
-    vim.wo.smartindent = original_smartindent
-  else
-    vim.api.nvim_input('<C-r>+')        -- Paste from the system clipboard
-  end
+  -- Force a redraw to apply the paste mode,
+  --  so the cursor will be at the right place after pasting
+  vim.cmd.redraw()
+
+  vim.api.nvim_input('<C-r>+')
+  vim.opt.paste = false
+
+  -- Reset autoindent and smartindent
+  vim.bo.autoindent  = original_autoindent
+  vim.bo.smartindent = original_smartindent
 end
 
 return M

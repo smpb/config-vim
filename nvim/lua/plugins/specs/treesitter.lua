b091f93ea3afa0ddbc81fad5b0fec01e9747cf0c
Sérgio Bernardino <me@sergiobernardino.net>
2026-04-07 20:54:02 +0100

    Updated 'nvim-treesitter' configuration for Neovim 0.12.

--- nvim/lua/plugins/specs/treesitter.lua
+++ nvim/lua/plugins/specs/treesitter.lua
@@ -1,27 +1,36 @@
+-- https://github.com/nvim-treesitter/nvim-treesitter/discussions/8621
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-
+    branch = 'main',
+    lazy   = false,
     build  = ':TSUpdate',
 
-    -- configuration
-    config = function ()
-      local configs = require('nvim-treesitter.configs')
+    config = function()
+      -- Ensure parsers are installed
+      require('nvim-treesitter').install({
+        'bash', 'c', 'comment', 'css', 'csv', 'diff', 'dockerfile',
+        'git_config', 'git_rebase', 'gitattributes', 'gitcommit', 'gitignore',
+        'go', 'gomod', 'gosum', 'gotmpl', 'helm', 'html', 'http', 'janet_simple',
+        'javascript', 'jq', 'json', 'lua', 'markdown', 'markdown_inline', 'make',
+        'perl', 'php', 'regex', 'sql', 'ssh_config', 'tmux', 'toml', 'typescript',
+        'vim', 'vimdoc', 'xml', 'yaml'
+      }):wait(300000)
 
-      configs.setup({
-        ensure_installed = {
-          'bash', 'c', 'comment', 'css', 'csv', 'diff', 'dockerfile',
-          'git_config', 'git_rebase', 'gitattributes', 'gitcommit', 'gitignore',
-          'go', 'gomod', 'gosum', 'gotmpl', 'helm', 'html', 'http', 'javascript',
-          'jq', 'json', 'lua', 'markdown', 'markdown_inline', 'make', 'perl',
-          'php', 'regex', 'sql', 'ssh_config', 'tmux', 'toml', 'typescript',
-          'vim', 'vimdoc', 'xml', 'yaml'
-        },
-        sync_install     = false,
-        highlight        = { enable = true },
-        indent           = { enable = true },
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(args)
+          -- Enable Tree-sitter highlighting via Neovim core
+          pcall(vim.treesitter.start, args.buf)
+
+          -- Enable folding
+          -- vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+          -- vim.wo[0][0].foldmethod = 'expr'
+
+          -- Enable indentation (best-effort: unstable in 0.12)
+          vim.bo.indentexpr = "v:lua.vim.treesitter.indentexpr()"
+        end,
       })
-    end
+    end,
   },
 
   {
